406e6d27118f6fdfb0ed3c23c9d1bea373cde53a
Quena <quenallatafranco@gmail.com>
2024-02-13 13:50:28 -0300

    provider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./main.scss";
+import store from "./redux/store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
