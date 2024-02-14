1037784c6d25bbea629a2a29ee70af9b0dd3e49e
nachof948 <nachof948@gmail.com>
2024-02-14 09:00:58 -0300

    SignUp

--- Client/src/views/SingUp/SignUp.jsx
+++ Client/src/views/SingUp/SignUp.jsx
@@ -8,7 +8,6 @@ const SignUp = () => {
   });
   
   const handleSignUp = async (values) => {
-    console.log(values)
     try{
       await axios.post('', values)
     }
@@ -20,32 +19,30 @@ const SignUp = () => {
 
 
   return(
-    <section style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
-      <h1 style={{fontSize:'4rem'}}>Registrate</h1>
+    <section className='signup-container'>
+      <h1>Registrate</h1>
       <form onSubmit={handleSubmit(handleSignUp)}>
-        <div style={{display:'flex', flexDirection:'column',alignItems:'flex-start', gap:'0.5rem'}}>
+        <div className='signup-form-inputs'>
 
           <label>Email</label>
-          <input style={{background:'lightgrey', width:'20rem', padding:'0.6rem 1rem', borderRadius:'7px'}}
+          <input 
           {...register('email',{required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
           type="text" name='email' placeholder='usuario@gmail.com' />
-          <div style={{color:'red'}}>
+          <div className='signup-error'>
             {errors.email?.type === 'required' && <p>El campo email es requerido</p>}
             {errors.email?.type === 'pattern' && <p>El correo electronico no es valido</p>}
           </div>
 
           <label>Contraseña</label>
-          <input style={{background:'lightgrey', width:'20rem', padding:'0.6rem 1rem',  borderRadius:'7px'}}
+          <input
           {...register('password',{required: true, minLength:8})}
           type="password" name='password' placeholder='xxxxxxxxxx' />
-          <div style={{color:'red'}}>
+          <div className='signup-error'>
             {errors.password?.type === 'required' && <p>El campo contraseña es requerido</p>}
             {errors.password?.type === 'minLength' && <p>La contraseña debe tener al menos 8 caracteres</p>}
           </div>
         </div>
-
         <button 
-        style={{background:'lightgrey', width:'8rem', padding:'0.6rem 1rem', borderRadius:'10px', marginTop:'1rem', cursor:'pointer'}}
         type='submit'>
           Registrarme
         </button>
