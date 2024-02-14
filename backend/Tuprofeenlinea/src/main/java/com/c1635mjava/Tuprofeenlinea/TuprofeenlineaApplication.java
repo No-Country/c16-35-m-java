7921487458b26af27f7dac6dbe58cc9efe2096be
Marco <marcovirinni@gmail.com>
2024-02-13 21:48:50 -0300

    Cambios en entorno

---
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/TuprofeenlineaApplication.java
@@ -0,0 +1,13 @@
+package com.c1635mjava.Tuprofeenlinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuprofeenlineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuprofeenlineaApplication.class, args);
+	}
+
+}
---
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/models/Client.java
@@ -0,0 +1,4 @@
+package com.c1635mjava.Tuprofeenlinea.models;
+
+public class Client {
+}
