baa180aefb6452bb15efbc17937c2f2b5f76ed02
Quena <quenallatafranco@gmail.com>
2024-02-19 17:41:46 -0300

    reubicando css

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../../main.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.scss";
 
 export const Nav = () => {
-  const openHamburguer = () => {
-
-  }
+  const openHamburguer = () => {};
   return (
-    <nav className='nav'>
-      <div className='logo'>
+    <nav className="nav">
+      <div className="logo">
         <Link to="/">LOGO</Link>
       </div>
-      <input type="text" className='searchbar' placeholder='¿Qué quieres aprender hoy?' />
+      <input
+        type="text"
+        className="searchbar"
+        placeholder="¿Qué quieres aprender hoy?"
+      />
       <ul>
-        <li><Link to="/signup">Creá tu cuenta</Link></li>
-        <li><Link to="login">Ingresá</Link></li>
-        <li><Link to="about">Acerca de</Link></li>
+        <li>
+          <Link to="/signup">Creá tu cuenta</Link>
+        </li>
+        <li>
+          <Link to="login">Ingresá</Link>
+        </li>
+        <li>
+          <Link to="about">Acerca de</Link>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
