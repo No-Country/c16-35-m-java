1aaab3371e07b087e9b1f6f9a770a32728f1d975
nachof948 <nachof948@gmail.com>
2024-02-20 22:39:20 -0300

    Cambios en el header y el nav

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from '../../assets/Logo.svg'
 import "./Nav.scss";
+
 
 export const Nav = () => {
   return (
+    <header>
     <nav className="nav">
       <div className="logo">
-        <Link to="/">LOGO</Link>
+        <Link to="/"><img src={Logo} alt="Logo de la aplicacion" /></Link>
       </div>
       <input
         type="text"
         className="searchbar"
         placeholder="¿Qué quieres aprender hoy?"
       />
-      <ul>
-        <li>
-          <Link to="/signup">Creá tu cuenta</Link>
-        </li>
-        <li>
-          <Link to="login">Ingresá</Link>
-        </li>
-        <li>
-          <Link to="about">Acerca de</Link>
-        </li>
-      </ul>
+      <div className="nav-enlaces">
+        <Link className="dar-clases" to="/about">Dar clases</Link>
+        <Link className="conectarse" to="/login">Conectarse</Link>
+      </div>
     </nav>
+    </header>
+
   );
 };
