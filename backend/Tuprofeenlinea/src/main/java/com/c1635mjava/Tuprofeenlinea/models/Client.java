91b183a6c35445434b1d043ecebd344c101a8ba6
David Alejandro Lugo Gámez <=>
2024-02-22 12:18:40 -0400

    Clases 01

--- backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/models/Client.java
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/models/Client.java
@@ -2,21 +2,30 @@ package com.c1635mjava.Tuprofeenlinea.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.List;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
 @Table(name = "client")
 public class Client {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String email;
     private String password;
+    private String name;
+    private String lastname;
+    private LocalDate birthday;
+    private String descriptionTeacher;
+    private String descriptionBiography;
     private boolean enabled;
+    @OneToMany(mappedBy = "student")
+    private List<Reservation> reservationList;
+    @OneToMany(mappedBy = "teacher")
+    private List<Calendary> teachingCalendaries;
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "role_id")
+    @JoinColumn(name = "role_Id")
     private Role role;
 }
---
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/models/Reservation.java
@@ -0,0 +1,23 @@
+package com.c1635mjava.Tuprofeenlinea.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "reservation")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private int duration;
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Client student;
+    @ManyToOne
+    @JoinColumn(name = "calendary_id")
+    private Calendary calendary;
+}
