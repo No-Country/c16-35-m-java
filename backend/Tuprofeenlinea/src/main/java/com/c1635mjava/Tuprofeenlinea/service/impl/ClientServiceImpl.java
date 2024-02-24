fae246767519650f6c629a21a83a2949c58f6559
David Alejandro Lugo Gámez <91472247+davidlugodev@users.noreply.github.com>
2024-02-24 00:11:42 -0400

    Update ClientServiceImpl.java
    client.setEnabled(true); //en save

--- backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/service/impl/ClientServiceImpl.java
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/service/impl/ClientServiceImpl.java
@@ -18,22 +18,6 @@ public class ClientServiceImpl implements IUserService {
     @Autowired
     private ClientRepository clientRepository;
 
-    @Override
-    public Client save(ClientDTO client) {
-        System.out.println("client: " + client);
-        return clientRepository.save(createClient(client));
-    }
-
-    private Client createClient(ClientDTO clientDTO){
-        Client client = new Client();
-
-        client.setEmail(clientDTO.getEmail());
-        client.setPassword(clientDTO.getPassword());
-        client.setEnabled(true);
-        client.setRole(new Role("TEACHER"));
-        return client;
-    }
-
     @GetMapping("/{id}")
     public Optional<Client> getClient(@PathVariable Long id) {
         return clientRepository.findById(id);
@@ -43,11 +27,13 @@ public class ClientServiceImpl implements IUserService {
     public List<Client> findAll() {
         return clientRepository.findAll();
     }
-    
+
     @Override
     public Client save(Client client) {
+        client.setEnabled(true);
         return clientRepository.save(client);
     }
+    
     @Override
     public Client update(Client client) {
         Client existingClient = clientRepository.
