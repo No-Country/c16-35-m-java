908da21dcfdb534afdd33e2ed8bc4dd3edb92c49
Marco <marcovirinni@gmail.com>
2024-03-09 12:51:16 -0300

    hasta aca se llego el MVP

---
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/models/MercadoPago.java
@@ -0,0 +1,22 @@
+//package com.c1635mjava.Tuprofeenlinea.models;
+//import com.mercadopago.MercadoPago;
+//
+//
+//import lombok.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class MercadoPago {
+//
+//    private String accessToken;
+//
+//    @Bean
+//    public MercadoPago.SDK configureMercadoPago() {
+//        MercadoPago.SDK sdk = new MercadoPago.SDK();
+//        sdk.configure(accessToken);
+//        return sdk;
+//    }
+//}
+//
---
+++ backend/Tuprofeenlinea/src/main/java/com/c1635mjava/Tuprofeenlinea/service/impl/MercadoPagoService.java
@@ -0,0 +1,20 @@
+//package com.c1635mjava.Tuprofeenlinea.service.impl;
+//
+//import com.mercadopago.MercadoPagoConfig;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MercadoPagoService {
+//
+//    @Autowired
+//    private MercadoPagoConfig mercadoPagoConfig;
+//
+//    public PaymentPreferenceResponse createPreference(PaymentPreferenceRequest request) throws Exception {
+//        MercadoPago.SDK mercadoPagoSdk = mercadoPagoConfig.configureMercadoPago();
+//
+//        // Implementa la lógica para crear la preferencia de pago utilizando el SDK de MercadoPago
+//
+//        return new PaymentPreferenceResponse(); // Reemplaza esto con la respuesta adecuada
+//    }
+//}
